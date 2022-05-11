6e787b337b2e156ff6e28315c06753fd82732d68
geki-kanagawa <105358782+geki-kanagawa@users.noreply.github.com>
2022-05-11 22:08:29 +0900

    Add files via upload

--- test/database/migrations/2022_04_28_074344_create_task_table.php
+++ test/database/migrations/2022_04_28_074344_create_task_table.php
@@ -13,11 +13,15 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('task', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('title');
-            $table->dateTime('datetime');
-            $table->string('memo');          
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->increments('id')->nullable(false);
+            $table->string('title')->nullable(false);
+            $table->dateTime('datetime')->nullable(false);
+            $table->text('memo');
+            $table->enum('category', ['private', 'work']);
+            $table->dateTime('ins_time')->nullable(false)->default('1900-01-01 00:00:00');
+            $table->dateTime('upd_time')->nullable(false)->default('1900-01-01 00:00:00');
+            $table->tinyInteger('flag')->nullable(false)->default(0);          
         });
     }
 
